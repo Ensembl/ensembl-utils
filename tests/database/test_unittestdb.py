d866223d6aa5390d4246f5bbec9e0175cde1e93b
Matthieu Barba <mbarba@ebi.ac.uk>
2024-06-27 17:39:09 +0100

    use session

--- tests/database/test_unittestdb.py
+++ tests/database/test_unittestdb.py
@@ -20,7 +20,7 @@ from typing import ContextManager, Optional
 
 import pytest
 from pytest import FixtureRequest, param, raises
-from sqlalchemy import VARCHAR
+from sqlalchemy import VARCHAR, text
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy.schema import MetaData
 from sqlalchemy_utils.functions import database_exists
@@ -88,8 +88,10 @@ class TestUnitTestDB:
                 assert db, "UnitTestDB should not be empty"
                 assert db.dbc, "UnitTestDB's database connection should not be empty"
                 # Check that the database has been loaded correctly from the dump files
-                result = db.dbc.execute("SELECT * FROM gibberish")
-                assert len(result.fetchall()) == 6, "Unexpected number of rows found in 'gibberish' table"
+
+                with db.dbc.test_session_scope() as session:
+                    result = session.execute(text("SELECT * FROM gibberish"))
+                    assert len(result.fetchall()) == 6, "Unexpected number of rows found in 'gibberish' table"
 
         @pytest.mark.parametrize(
             "metadata, tables",
